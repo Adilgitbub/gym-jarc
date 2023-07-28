e0a5f3f499eddf1e63e4dcec43c16a546b84128c
Adilgitbub <adilshaikh5991@gmail.com>
2023-07-29 00:03:57 +0530

    project setup intial commit

---
+++ services/user-tenant-service/src/main/java/com/gym/jarc/usertenantservice/UserTenantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gym.jarc.usertenantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTenantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTenantServiceApplication.class, args);
+	}
+
+}
---
+++ services/user-tenant-service/src/test/java/com/gym/jarc/usertenantservice/UserTenantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.jarc.usertenantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTenantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
